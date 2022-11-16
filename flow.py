d781ab32931bc79609d6b49251f564b5866fd1ab
DerFahrgast <13707988+DerFahrgast@users.noreply.github.com>
2022-11-16 12:46:32 +0800

    modified:     flow.py

--- flow.py
+++ flow.py
@@ -30,13 +30,13 @@ def NeuralODE(vec_field_net, sample_size, dim):
         '''
         flow from x0 to x1
         '''
-        def f(t, y, params):
+        def f(t, y, args):
             return vmap(vec_field_net, (None, 0, None), 0)(params, y, t)
 
         term = ODETerm(f)
         solver = Dopri5()
         y0 = random.normal(rng, (sample_size, dim))
-        solution = diffeqsolve(term, solver, t0=0, t1=1, y0=y0, max_steps=100)
+        solution = diffeqsolve(term, solver, t0=0, t1=1, dt0=0.1, y0=y0, max_steps=100)
 
         return solution.ys
 
@@ -48,14 +48,14 @@ def NeuralODE(vec_field_net, sample_size, dim):
         def base_logp(x):
             return norm.logpdf(x).sum(-1)
 
-        def f(t, y, params):
+        def f(t, y, args):
             return [-vec_field_net(params, y[0], t), divergence_fwd(vec_field_net)(params, y[0], t)]
 
         logp = 0.0
 
         term = ODETerm(f)
         solver = Dopri5()
-        solution = diffeqsolve(term, solver, t0=0, t1=1, y0=[x, logp], max_steps=100)
+        solution = diffeqsolve(term, solver, t0=0, t1=1, dt0=0.1, y0=[x, logp], max_steps=100)
         
         return - solution.ys[1] + base_logp(solution.ys[0])
 
@@ -68,4 +68,26 @@ def NeuralODE(vec_field_net, sample_size, dim):
     return batched_sampler, logp
 
 if __name__ == '__main__':
-    pass
+    from jax.example_libraries.stax import serial, Dense, Relu
+    from jax.nn.initializers import zeros
+
+    dim = 2
+    sample_size = 10
+
+    def make_vec_field_net(rng):
+        net_init, net_apply = serial(Dense(8), Dense(dim, W_init=zeros, b_init=zeros))
+        in_shape = (-1, dim+1)
+        _, net_params = net_init(rng, in_shape)
+
+        def net_with_t(params, x, t):
+            return net_apply(params, jnp.concatenate((x,t.reshape(1))))
+        
+        return net_params, net_with_t
+
+    init_rng, rng = random.split(random.PRNGKey(42))
+    params, vec_field_net = make_vec_field_net(init_rng)
+
+    batched_sampler, batched_logp = NeuralODE(vec_field_net, sample_size, dim)
+
+    print(batched_sampler(rng, params).shape)
+
